493f824473fbfaeda2fecc6ba6dfe8fc97a2104a
Konstantin Lysyy <kesishere@gmail.com>
2026-06-12 13:00:45 +0300

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun hasDuplicate(nums: IntArray): Boolean {
+        val set = hashSetOf<Int>()
+        for(num in nums){
+            if(set.contains(num)) return true
+            set.add(num)
+        }
+
+        return false
+    }
+}
